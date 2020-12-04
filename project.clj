f130aa5d46d85f62cae059788ab568e6516da2f0
Stu W <stubillwhite@gmail.com>
2020-12-04 18:36:59 +0000

    Solution to day four part two

--- project.clj
+++ project.clj
@@ -11,10 +11,18 @@
 
   :plugins []
   
-  :dependencies [[org.clojure/tools.nrepl "0.2.13"]
+  :dependencies [;; core
+                 [org.clojure/tools.nrepl "0.2.13"]
                  [org.clojure/clojure "1.10.1"]
-                 [com.taoensso/timbre "5.1.0"]
                  [org.clojure/tools.trace "0.7.10"]
+
+                 ;; Logging
+                 [com.taoensso/timbre "5.1.0"]
+
+                 ;; Spec helpers
+                 [expound "0.7.2"]
+
+                 ;; DI
                  [mount "0.1.16"]]
   
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.0.0"]]
--- src/advent_of_code_2020/day_4.clj
+++ src/advent_of_code_2020/day_4.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code-2020.day-4
-  (:require [advent-of-code-2020.utils :refer [def- parse-long]]
+  (:require [advent-of-code-2020.utils :refer [parse-long]]
             [clojure.java.io :as io]
+            [clojure.spec.alpha :as spec]
             [clojure.string :as string]))
 
 (def problem-input
@@ -9,29 +10,57 @@
 (defn- parse-record [record]
   (let [passport-regex #"([a-z]+):(\S+)"
         matches        (re-seq passport-regex record)]
-    (into {} (for [[_ k v] matches] [k v]))))
+    (into {} (for [[_ k v] matches] [(keyword k) v]))))
 
 (defn parse-input [input]
   (->> (string/split input #"(?m)^\n")
        (map parse-record)
        (into [])))
 
-(def- expected-fields
-  ["byr"
-   "iyr"
-   "eyr"
-   "hgt"
-   "hcl"
-   "ecl"
-   "pid"
-   ;; "cid" -- treat as optional
-   ])
-
-(defn- valid-passport? [record]
-  (every? (fn [k] (contains? record k)) expected-fields))
+(defn- has-valid-passport-fields? [record]
+  (let [expected-fields [:byr :iyr :eyr :hgt :hcl :ecl :pid]]
+    (every? (fn [k] (contains? record k)) expected-fields)))
 
 (defn solution-part-one [input]
   (->> input
        (parse-input)
+       (filter has-valid-passport-fields?)
+       (count)))
+
+;; Part two
+
+(defn- height? [s]
+  (when-let [[m] (re-seq #"(\d+)(in|cm)" s)]
+    (let [[_ v unit] m
+          value      (parse-long v)]
+      (or (and (= "cm" unit) (<= 150 value 193))
+          (and (= "in" unit) (<= 59  value 76))))))
+
+(defn- year-between-inclusive [min max]
+  (fn [s]
+    (and (re-matches #"[0-9]{4}" s)
+         (<= min (parse-long s) (inc max)))))
+
+(defn- year? [s]
+  (re-matches #"[0-9]{4}" s))
+
+(spec/def ::byr (year-between-inclusive 1920 2002))
+(spec/def ::iyr (year-between-inclusive 2010 2020))
+(spec/def ::eyr (year-between-inclusive 2020 2030))
+(spec/def ::hgt (spec/and string? height?))
+(spec/def ::hcl (spec/and string? #(re-matches #"#[0-9a-f]{6}" %)))
+(spec/def ::ecl #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"})
+(spec/def ::pid (spec/and string? #(re-matches #"[0-9]{9}" %)))
+
+(spec/def ::passport
+  (spec/keys :req-un [::byr ::iyr ::eyr ::hgt ::hcl ::ecl ::pid]))
+
+(defn- valid-passport? [record]
+  (spec/valid? ::passport record))
+
+(defn solution-part-two [input]
+  (->> input
+       (parse-input)
        (filter valid-passport?)
        (count)))
+
