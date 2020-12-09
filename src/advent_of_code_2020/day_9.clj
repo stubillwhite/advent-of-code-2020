db5e7afd628d89f1a7b93925a54fad66f374e45c
Stu W <stubillwhite@gmail.com>
2020-12-09 12:57:22 +0000

    Solution to day nine part two

--- src/advent_of_code_2020/day_9.clj
+++ src/advent_of_code_2020/day_9.clj
@@ -35,3 +35,31 @@
        (first)
        (first)))
 
+;; Part two
+
+(defn- take-until-totalling [total xs]
+  (loop [remaining    (rest xs)
+         subseq       [(first xs)]
+         subseq-total (first xs)]
+    (cond
+      (= total subseq-total) subseq
+      (< total subseq-total) nil
+      :else                  (recur (rest remaining)
+                                    (conj subseq (first remaining))
+                                    (+ subseq-total (first remaining))))))
+
+(defn contiguous-sequence-totalling [total xs]
+  (loop [xs xs]
+    (if-let [range (take-until-totalling total xs)]
+      range
+      (recur (rest xs)))))
+
+(defn- sum-of-smallest-and-largest [xs]
+  (+ (apply min xs) (apply max xs)))
+
+(defn solution-part-two [input length]
+  (let [total (solution-part-one input length)]
+    (->> (parse-input input)
+         (contiguous-sequence-totalling total)
+         (sum-of-smallest-and-largest))))
+
