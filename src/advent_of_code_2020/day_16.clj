66111646d0075f068621fc61f7ac2fd5ca892a71
Stu W <stubillwhite@gmail.com>
2020-12-28 18:23:49 +0000

    Solution to day sixteen part two

--- src/advent_of_code_2020/day_16.clj
+++ src/advent_of_code_2020/day_16.clj
@@ -6,17 +6,23 @@
 (def problem-input
   (string/trim (slurp (io/resource "day-16-input.txt"))))
 
-(defn- create-predicate [s]
+;; Parsing
+
+(defn- create-validator [s]
   (let [[min max] (map parse-long (string/split s #"-"))]
     (fn [x] (<= min x max))))
 
-(defn- create-field [s]
+(defn- create-field-validator [s]
   (let [[field constraints] (string/split s #": ")
-        validator           (apply some-fn (map create-predicate (string/split constraints #" or ")))]
-    {(keyword field) validator}))
+        validator           (apply some-fn (map create-validator (string/split constraints #" or ")))]
+    {(keyword (string/replace field " " "-")) validator}))
 
 (defn- parse-top [s]
-  (apply merge (map create-field (string/split-lines s))))
+  (apply merge (map create-field-validator (string/split-lines s))))
+
+(defn- parse-middle [s]
+  (let [csv (fnext (string/split-lines s))]
+    (into [] (map parse-long (string/split csv #",")))))
 
 (defn- parse-bottom [s]
   (let [lines (->> (string/split-lines s) (rest))]
@@ -24,18 +30,77 @@
          (map (fn [x] (into [] (map parse-long (string/split x #",")))))
          (into []))))
 
-(defn- parse-input [input]
+(defn parse-input [input]
   (let [[top middle bottom] (string/split input #"(?m)^$\n")]
-    {:fields         (parse-top top)
-     :nearby-tickets (parse-bottom bottom)}))
+    {:field-validators (parse-top top)
+     :your-ticket      (parse-middle middle)
+     :nearby-tickets   (parse-bottom bottom)}))
+
+;; Part one
+
+(defn- valid-ticket? [field-validators ticket]
+  (let [valid-for-some-field? (apply some-fn (vals field-validators))]
+    (every? valid-for-some-field? ticket)))
+
+(defn parse-and-categorise-tickets [input]
+  (let [{:keys [field-validators your-ticket nearby-tickets]} (parse-input input)
+        categorised-tickets (->> nearby-tickets (group-by (partial valid-ticket? field-validators)))]
+    {:field-validators       field-validators
+     :your-ticket            your-ticket
+     :nearby-valid-tickets   (get categorised-tickets true  [])
+     :nearby-invalid-tickets (get categorised-tickets false [])}))
 
 (defn solution-part-one [input]
-  (let [{:keys [fields nearby-tickets]} (parse-input input)
-        satisfies-none?                 (complement (apply some-fn (vals fields)))]
-    (->> nearby-tickets
+  (let [{:keys [field-validators nearby-invalid-tickets]} (parse-and-categorise-tickets input)
+        satisfies-none? (complement (apply some-fn (vals field-validators)))]
+    (->> nearby-invalid-tickets
          (flatten)
          (filter satisfies-none?)
          (apply +))))
 
+;; Part two
 
+(defn find-fields-which-fit-ticket-values [{:keys [field-validators your-ticket nearby-valid-tickets]}]
+  (let [all-tickets     (concat [your-ticket] nearby-valid-tickets)
+        idx-field-pairs (for [[field pred] field-validators
+                              idx          (range (count your-ticket))
+                              :when (every? pred (map (fn [x] (nth x idx)) all-tickets))]
+                          [idx field])]
+    (into {}
+          (for [[k vs] (group-by first idx-field-pairs)]
+            [k (into #{} (map last vs))]))))
+
+(defn- remove-name [by-idx name]
+  (into {}
+        (for [[idx names] by-idx
+              :when (not= #{name} names)]
+          [idx (disj names name)])))
+
+(defn resolve-fields
+  ([by-idx]
+   (resolve-fields by-idx (into [] (repeat (count (keys by-idx)) nil))))
+
+  ([by-idx fields]
+   (if (or (empty? by-idx) (every? identity fields))
+     fields
+     (let [idx            (first (first (sort-by (fn [[k v]] (count v)) by-idx)))
+           [name & names] (get by-idx idx)]
+       (if (nil? names)
+         (resolve-fields (remove-name (dissoc by-idx idx) name) (assoc fields idx name))
+         (or (resolve-fields (remove-name (dissoc by-idx idx) name) (assoc fields idx name))
+             (resolve-fields (remove-name (dissoc by-idx idx) name) fields)))))))
+
+(defn- product-of-departure-fields [your-ticket resolved-fields]
+  (let [field-indexes (for [f resolved-fields
+                            :when (string/starts-with? (name f) "departure")]
+                        (.indexOf resolved-fields f))]
+    (->> field-indexes
+         (map (partial get your-ticket))
+         (apply *))))
+
+(defn solution-part-two [input]
+  (let [parsed-input     (parse-and-categorise-tickets input)
+        possible-fields  (find-fields-which-fit-ticket-values parsed-input)
+        resolved-fields  (resolve-fields possible-fields)]
+    (product-of-departure-fields (:your-ticket parsed-input) resolved-fields)))
 
--- test/advent_of_code_2020/day_16_test.clj
+++ test/advent_of_code_2020/day_16_test.clj
@@ -4,7 +4,7 @@
             [clojure.string :as string]
             [clojure.test :refer :all]))
 
-(def example-input
+(def- example-input
   (string/join "\n"
                ["class: 1-3 or 5-7"
                 "row: 6-11 or 33-44"
@@ -24,3 +24,32 @@
 
 (deftest solution-part-one-given-problem-input-then-correct-result
   (is (= 21978 (solution-part-one problem-input))))
+
+(def- small-input
+  (string/join "\n"
+               ["class: 0-1 or 4-19"
+                "row: 0-5 or 8-19"
+                "seat: 0-13 or 16-19"
+                ""
+                "your ticket:"
+                "11,12,13"
+                ""
+                "nearby tickets:"
+                "3,9,18"
+                "15,1,5"
+                "5,14,9"]))
+
+(deftest find-fields-which-fit-ticket-values-given-small-input-then-potential-matches
+  (let [parsed-input (parse-and-categorise-tickets small-input)
+        expected     {0 #{:row}
+                      1 #{:class :row}
+                      2 #{:class :row :seat}}]
+    (is (= expected (find-fields-which-fit-ticket-values parsed-input)))))
+
+(deftest resolve-fields-given-solvable-fields-then-resolves
+  (let [fields-by-idx {0 #{:foo :qux} 1 #{:bar :foo :qux} 2 #{:qux}}]
+    (is (= [:foo :bar :qux] (resolve-fields fields-by-idx)))))
+
+(deftest solution-part-two-given-problem-input-then-correct-result
+  (is (= 1053686852011 (solution-part-two problem-input))))
+
