f4c8c0713b113411548449a00754d86d34597f1a
Stu W <stubillwhite@gmail.com>
2020-12-30 00:33:36 +0000

    Solution to day seventeen part two

--- src/advent_of_code_2020/day_17.clj
+++ src/advent_of_code_2020/day_17.clj
@@ -19,9 +19,9 @@
     (into {}
           (for [y (range height)
                 x (range width)]
-            [[x y 0] (case (get-in lines [y x])
-                       \. :inactive
-                       \# :active)]))))
+            [[x y 0 0] (case (get-in lines [y x])
+                         \. :inactive
+                         \# :active)]))))
 
 (def- example-input
     (string/join "\n"
@@ -29,39 +29,54 @@
                   "..#"
                   "###"]))
 
-(defn- neighbours [[x y z]]
+(defn neighbours-3d [[x y z w]]
   (for [dx (range -1 2)
         dy (range -1 2)
         dz (range -1 2)
-        :when (not= [0 0 0] [dx dy dz])]
-    (map + [x y z] [dx dy dz])))
+        :when (not= [0 0 0 0] [dx dy dz 0])]
+    (map + [x y z w] [dx dy dz 0])))
 
 (defn- states [f-next state]
   (iterate f-next state))
 
-(defn- expand-area [state]
+(defn- expand-area [f-neighbours state]
   (apply merge
          (for [[location status] state]
-           (into {} (for [k (neighbours location)] [k :inactive])))))
+           (into {} (for [k (f-neighbours location)] [k :inactive])))))
 
-(defn- active-neighbours [state location]
-  (let [by-state (group-by (fn [[loc state]] state) (select-keys state (neighbours location)))]
+(defn- active-neighbours [f-neighbours state location]
+  (let [by-state (group-by (fn [[loc state]] state) (select-keys state (f-neighbours location)))]
     (get by-state :active)))
 
-(defn next-state [curr-state]
+(defn next-state [f-neighbours curr-state]
   (into {}
-        (for [[location status] (merge (expand-area curr-state) curr-state)]
-          (let [active-count (count (active-neighbours curr-state location))
+        (for [[location status] (merge (expand-area f-neighbours curr-state) curr-state)]
+          (let [active-count (count (active-neighbours f-neighbours curr-state location))
                 new-status   (cond
                                (and (= status :active)   (<= 2 active-count 3)) :active
                                (and (= status :inactive) (= 3 active-count))    :active
                                :else                                            :inactive)]
             [location new-status]))))
 
-(defn- states [initial-state]
-  (iterate next-state initial-state))
+(defn- states [f-neighbours initial-state]
+  (iterate (partial next-state f-neighbours) initial-state))
 
 (defn solution-part-one [input]
-  (->> (nth (states (parse-input input)) 6)
+  (->> (nth (states neighbours-3d (parse-input input)) 6)
+       (filter (fn [[location status]] (= status :active)))
+       (count)))
+
+;; Part two
+
+(defn neighbours-4d [[x y z w]]
+  (for [dx (range -1 2)
+        dy (range -1 2)
+        dz (range -1 2)
+        dw (range -1 2)
+        :when (not= [0 0 0 0] [dx dy dz dw])]
+    (map + [x y z w] [dx dy dz dw])))
+
+(defn solution-part-two [input]
+  (->> (nth (states neighbours-4d (parse-input input)) 6)
        (filter (fn [[location status]] (= status :active)))
        (count)))
--- test/advent_of_code_2020/day_17_test.clj
+++ test/advent_of_code_2020/day_17_test.clj
@@ -4,23 +4,24 @@
             [clojure.string :as string]
             [clojure.test :refer :all]))
 
-(def example-input
+(def- example-input
     (string/join "\n"
                  [".#."
                   "..#"
                   "###"]))
 
 (defn- to-display [state]
-  (let [xs (distinct (sort (map first (keys state))))
-        ys (distinct (sort (map fnext (keys state))))
-        zs (distinct (sort (map last (keys state))))]
+  (let [coords-of-axis (fn [n] (distinct (sort (map #(nth % n) (keys state)))))
+        xs             (coords-of-axis 0) 
+        ys             (coords-of-axis 1) 
+        zs             (coords-of-axis 2)]
     (string/join "\n\n"
                  (for [z zs]
                    (str "z=" z "\n"
                         (->> (for [y ys
                                    x xs]
                                (case (get state
-                                          [x y z])
+                                          [x y z 0])
                                  :active   "#"
                                  :inactive "."))
                              (partition (count xs))
@@ -28,7 +29,7 @@
                              (string/join "\n")))))))
 
 (defn- nth-state [input n]
-  (-> (iterate next-state (parse-input input))
+  (-> (iterate (partial next-state neighbours-3d) (parse-input input))
       (nth n)
       (to-display)))
 
@@ -73,3 +74,8 @@
 (deftest solution-part-one-given-problem-input-then-correct-result
   (is (= 267 (solution-part-one problem-input))))
 
+(deftest solution-part-two-given-example-input-then-example-result
+  (is (= 848 (solution-part-two example-input))))
+
+(deftest solution-part-two-given-problem-input-then-problem-result
+  (is (= 1812 (solution-part-two problem-input))))
